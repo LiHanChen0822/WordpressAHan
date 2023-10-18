3abe6041e57f824f99172c191ff72c3ed114418d
LiHan <encorex32268@gmail.com>
2023-10-18 21:00:23 +0900

    Initial commit

---
+++ app/src/main/java/lihan/chen/wordpressahan/MainActivity.kt
@@ -0,0 +1,25 @@
+package lihan.chen.wordpressahan
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import lihan.chen.wordpressahan.feature.norippleclick.NoRippleClick
+import lihan.chen.wordpressahan.ui.theme.WordpressAHanTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WordpressAHanTheme {
+                NoRippleClick()
+            }
+        }
+    }
+}
